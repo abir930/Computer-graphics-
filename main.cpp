0b5711d3d266a229e85e7c45dc6267a09b9c3b09
abir930 <abirhasanmahin228@gmail.com>
2026-04-21 15:58:57 +0600

    updated main function with parts leveling

--- main.cpp
+++ main.cpp
@@ -1,170 +1,142 @@
+
 #include <GL/glut.h>
 #include <math.h>
+#include <stdlib.h>
+#include <stdio.h>
 
-#define PI 3.1416
+#include "globals.h"
+#include "algorithms.h"
+#include "planets.h"
+#include "animation.h"
+#include "stars.h"
+#include "comets.h"
+#include "hud.h"
 
-// Global Variables
-float earthAngle = 0.0f;
-float moonAngle  = 0.0f;
 
-// Initialization
-void init()
+float angMercury = 0;
+float angVenus   = 0;
+float angEarth   = 0;
+float angMars    = 0;
+float angJupiter = 0;
+float angSaturn  = 0;
+float angUranus  = 0;
+float angNeptune = 0;
+float angMoon    = 0;
+float sunPulse   = 0;
+
+float camAz   = 30;
+float camEl   = 25;
+float camZoom = 38;
+
+int mouseBtn = -1;
+int mouseX   = 0;
+int mouseY   = 0;
+
+float starX[STAR_COUNT];
+float starY[STAR_COUNT];
+float starZ[STAR_COUNT];
+float starBright[STAR_COUNT];
+int   starType[STAR_COUNT];
+
+float debX[DEBRIS_COUNT];
+float debY[DEBRIS_COUNT];
+float debZ[DEBRIS_COUNT];
+float debOrbR[DEBRIS_COUNT];
+float debOrbAng[DEBRIS_COUNT];
+float debSpeed[DEBRIS_COUNT];
+float debSize[DEBRIS_COUNT];
+float debR[DEBRIS_COUNT];
+float debG[DEBRIS_COUNT];
+float debB[DEBRIS_COUNT];
+
+Comet comets[COMET_COUNT];
+
+
+float rf(float lo, float hi)
 {
-    glClearColor(0.0, 0.0, 0.05, 1.0);
-    glEnable(GL_DEPTH_TEST);
+    return lo + ((float)rand() / RAND_MAX) * (hi - lo);
 }
 
-// DDA Line Algorithm
-void DDA_Line(float x1, float y1, float z1,
-              float x2, float y2, float z2)
-{
-    float dx = x2 - x1;
-    float dy = y2 - y1;
-    float dz = z2 - z1;
 
-    float steps = fabs(dx);
-    if (fabs(dy) > steps) steps = fabs(dy);
-    if (fabs(dz) > steps) steps = fabs(dz);
-
-    float xi = dx / steps;
-    float yi = dy / steps;
-    float zi = dz / steps;
-
-    float x = x1;
-    float y = y1;
-    float z = z1;
-
-    glBegin(GL_POINTS);
-    for (int i = 0; i <= steps; i++)
-    {
-        glVertex3f(x, y, z);
-        x += xi;
-        y += yi;
-        z += zi;
-    }
-    glEnd();
-}
-
-//  Orbit using DDA
-void drawOrbitDDA(float radius)
-{
-    glColor3f(0.6, 0.6, 0.6);
-
-    float prevX = radius;
-    float prevZ = 0;
-
-    for (int i = 1; i <= 360; i++)
-    {
-        float angle = i * PI / 180.0;
-        float x = radius * cos(angle);
-        float z = radius * sin(angle);
-
-        DDA_Line(prevX, 0, prevZ, x, 0, z);
-
-        prevX = x;
-        prevZ = z;
-    }
-}
-
-//  Draw Planet
-void drawPlanet(float distance, float angle, float r, float g, float b, float size)
-{
-    glPushMatrix();
-        glRotatef(angle, 0, 1, 0);
-        glTranslatef(distance, 0, 0);
-        glColor3f(r, g, b);
-        glutSolidSphere(size, 30, 30);
-    glPopMatrix();
-}
-
-// Draw Moon
-void drawMoon(float distance, float angle)
-{
-    glPushMatrix();
-        glRotatef(angle, 0, 1, 0);
-        glTranslatef(distance, 0, 0);
-        glColor3f(0.8, 0.8, 0.8);
-        glutSolidSphere(0.2, 20, 20);
-    glPopMatrix();
-}
-
-// Scene
-void display()
+void display(void)
 {
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     glLoadIdentity();
 
-    gluLookAt(0, 6, 12,
-              0, 0, 0,
-              0, 1, 0);
+    /* Position camera */
+    float cx, cy, cz;
+    getCamPos(&cx, &cy, &cz);
+    gluLookAt(cx, cy, cz, 0, 0, 0, 0, 1, 0);
 
-    //  Sun
-    glColor3f(1.0, 0.8, 0.0);
-    glutSolidSphere(1.5, 40, 40);
+    /* ── Shykha: background stars ── */
+    drawStars();
 
-    //Orbits using DDA
-    drawOrbitDDA(4.0);
-    drawOrbitDDA(7.0);
+    /* ── Abir: reference axes + orbit rings ── */
+    drawAxes();
+    drawAllOrbits();
 
-    //Earth + Moon
-    glPushMatrix();
-        glRotatef(earthAngle, 0, 1, 0);
-        glTranslatef(4.0, 0, 0);
+    /* ── Kabbo: sun + all planets ── */
+    drawSun();
+    drawAllPlanets();
 
-        glColor3f(0.2, 0.5, 1.0);
-        glutSolidSphere(0.5, 30, 30);
+    /* ── Shykha: asteroid belt + comets ── */
+    drawDebris();
+    drawComets();
 
-        drawOrbitDDA(1.2);
-        drawMoon(1.2, moonAngle);
-
-    glPopMatrix();
-
-    // Another Planet
-    drawPlanet(7.0, earthAngle * 0.5, 1.0, 0.4, 0.2, 0.6);
+    /* ── Shykha: HUD overlay ── */
+    drawHUD();
 
     glutSwapBuffers();
 }
 
-//Animation
-void update(int value)
+
+void init(void)
 {
-    earthAngle += 1.0f;
-    moonAngle  += 3.0f;
+    glEnable(GL_DEPTH_TEST);
+    glEnable(GL_LIGHTING);
+    glEnable(GL_LIGHT0);
+    glEnable(GL_COLOR_MATERIAL);
+    glColorMaterial(GL_FRONT, GL_AMBIENT_AND_DIFFUSE);
+    glEnable(GL_BLEND);
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 
-    if (earthAngle > 360) earthAngle -= 360;
-    if (moonAngle  > 360) moonAngle  -= 360;
+    float amb[] = {0.1f, 0.1f, 0.1f, 1.0f};
+    float dif[] = {1.0f, 1.0f, 1.0f, 1.0f};
+    float lp[]  = {0.0f, 0.0f, 0.0f, 1.0f};
+    glLightfv(GL_LIGHT0, GL_AMBIENT,  amb);
+    glLightfv(GL_LIGHT0, GL_DIFFUSE,  dif);
+    glLightfv(GL_LIGHT0, GL_POSITION, lp);
 
-    glutPostRedisplay();
-    glutTimerFunc(16, update, 0);
+    glClearColor(0.0f, 0.0f, 0.03f, 1.0f);
+
+    /* Initialise subsystems */
+    initStars();
+    initDebris();
+    initComets();
+
+    printf("\n=== Solar System Simulation ===\n");
+    printf("Team: Abir | Kabbo | Shykha\n");
+    printf("Left-Drag: Rotate | Right-Drag: Zoom | Middle: Reset\n");
+    printf("Stars: %d  |  Debris: %d  |  Comets: %d\n",
+           STAR_COUNT, DEBRIS_COUNT, COMET_COUNT);
+    printf("Algorithms: DDA | Bresenham | Midpoint Circle | 2D/3D Transform\n\n");
 }
 
-// Reshape
-void reshape(int w, int h)
-{
-    if (h == 0) h = 1;
-
-    glViewport(0, 0, w, h);
-
-    glMatrixMode(GL_PROJECTION);
-    glLoadIdentity();
-    gluPerspective(60, (float)w / h, 1, 100);
-
-    glMatrixMode(GL_MODELVIEW);
-}
-
-// -------------------- Main --------------------
-int main(int argc, char** argv)
+int main(int argc, char **argv)
 {
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
-
-    glutInitWindowSize(900, 650);
-    glutCreateWindow("Solar System - DDA Version");
+    glutInitWindowSize(1100, 750);
+    glutCreateWindow(
+        "Solar System -- Abir | Kabbo | Shykha  --  "
+        "DDA | Bresenham | Midpoint | 2D/3D Transform");
 
     init();
 
     glutDisplayFunc(display);
     glutReshapeFunc(reshape);
+    glutMouseFunc(onMouseBtn);
+    glutMotionFunc(onMouseMove);
     glutTimerFunc(0, update, 0);
 
     glutMainLoop();
